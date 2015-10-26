2b5a53390b635fcb8d412e6a3a0c474a92feb158
Miriam Wodrich <miriam.wodrich@f1006-05-34128.bmcc.cuny.edu>
2015-10-26 15:23:40 -0400

    adding circles to an array

--- week7-inclass/js/main.js
+++ week7-inclass/js/main.js
@@ -5,5 +5,31 @@ function setup(){
     var cnv = createCanvas(800,3000);
     cnv.parent("windows");
     
+    var myArray = [];
+    //add stuff to the array using a loop
+    //create an array
+    for(var i=0; i<100; i++) {
+//        myArray[i] = "item" + i;
+//        myArray[i] = i;
+        //this adds circles to the array but we wont be able to address individual cirlces
+        myArray[i] = ellipse(i, i, 10, 10);
+    }
+    console.log(myArray);
+    
+    
+    
+    
+    // loop to go through every item in the array and add 10 to each item
+    // if item is a string it will just get concatenated, but if item is a number // it will actually get added
+    
+    for(var i=0; i< myArray.length; i++) {
+        myArray[i] += 10;
+//        console.log(myArray[i]);
+    }
+    
+    //log out the array
+    console.log(myArray);
+    
+    
 }//end setup
 
