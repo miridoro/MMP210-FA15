2e447bc7e9f5808fd0eae478b6cd73854438ffeb
Miriam Wodrich <miriam.wodrich@f1006-05-34128.bmcc.cuny.edu>
2015-10-26 16:25:53 -0400

    adding clicking a bubble

--- week7-inclass/js/main.js
+++ week7-inclass/js/main.js
@@ -1,8 +1,26 @@
 //global variables
+var myArray = []; 
+
+//bubble constructor
+function Bubble() {
+    // create x and y properties (variables)
+    this.x = random(width); //width is width of canvas
+    this.y = random(height);
+    this.r = 20; //r is for radius which is 1/2 the width of the circle
+    this.bubbleSize = this.r*2;  //width and height are twice the radius
+    this.c = fill('yellow');
+    
+    //method: draw the bubble
+    this.drawBubble = function(){
+    ellipse(this.x, this.y, this.bubbleSize, this.bubbleSize);
+    }
+    
+    
+} //end bubble
 
 function setup(){
     //create the canvas. put into a variable to associate with a element
-    var cnv = createCanvas(800,3000);
+    var cnv = createCanvas(windowWidth,windowHeight);
     cnv.parent("windows");
     
     var myArray = [];
@@ -11,25 +29,50 @@ function setup(){
     for(var i=0; i<100; i++) {
 //        myArray[i] = "item" + i;
 //        myArray[i] = i;
+        
         //this adds circles to the array but we wont be able to address individual cirlces
-        myArray[i] = ellipse(i, i, 10, 10);
+//       
+          myArray[i] = new Bubble();
+          myArray[i].drawBubble();
+        
     }
     console.log(myArray);
     
     
     
-    
     // loop to go through every item in the array and add 10 to each item
-    // if item is a string it will just get concatenated, but if item is a number // it will actually get added
+    // if item is a string it will just get concatenated, but if item is a number it will actually get added
+    
+    
     
     for(var i=0; i< myArray.length; i++) {
-        myArray[i] += 10;
-//        console.log(myArray[i]);
+        // if dont comment this out, then wont be able to see coordinates in console  
+        //        myArray[i] += 10;
+        console.log(myArray[i]);
     }
     
     //log out the array
     console.log(myArray);
     
     
-}//end setup
+    
+    
+} //end setup
 
+//listen for mouse presses with P5 mousePressed method
+function mousePressed() {
+    //check this is working
+    console.log("mouse Pressed");
+    
+    //loop through the bubbles and see if mouse inside
+    
+    for(var i=0; i< myArray.length; i++) {
+        // if dont comment this out, then wont be able to see coordinates in console  
+        //        myArray[i] += 10;
+        
+        
+        console.log(myArray);
+    }
+    
+    
+}
