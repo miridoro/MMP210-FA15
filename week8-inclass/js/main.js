ae153ee008f47f99b937ae3ca65cc3dea90a64cb
Miriam <miriam.wodrich@gmail.com>
2015-11-14 13:19:13 -0500

    first finished draft of midterm

--- week8-inclass/js/main.js
+++ week8-inclass/js/main.js
@@ -1,18 +1,24 @@
+//define global variables
 var cicleSize;
 var circleX, circleY;
 var x, y;
+//variable that sets state of rotation, 0 stands for 'off' and 1 stands for 'on'
 var rot = 0;
+var buttonState = 1;
+var buttonState2 = 0;
+
 
 
 function setup() {
     //create the Canvas
     
     createCanvas(windowWidth, windowHeight);
-   
     
 }
 
-function drawButton() {
+
+//function to draw the 'Play' button
+function drawButton(buttonState) {
     cirlceX = 120 + 70;
     circleY = 50;
     circleSize = 80;
@@ -25,7 +31,8 @@ function drawButton() {
     text("Press Play", 15, 55);
 }
 
-function drawButton2() {
+//function to draw the 'Pause' button
+function drawButton2(buttonState2) {
     noStroke();
     ellipse(120 + 70, 50, 80, 80);
     fill('black');
@@ -35,75 +42,73 @@ function drawButton2() {
     text("Press Pause", 15, 55);
 }
 
+//function that calls and runs drawButton() and drawPattern() functions 
 function draw() {
-     background('orange');
+    background('orange');
     fill('white');
-    drawButton();
+    
+    if(buttonState){
+    drawButton(buttonState);}
 //    drawButton2();
-    
-    
-  
-    
-   
 
-    //drawPattern(300, 300);
+    if(buttonState2){
+        drawButton2(buttonState2);
+    }
     
 
     //console.log("gridwidth: " + gridwidth)
     
+    //checking if loop gets run
     console.log("start loop");
     
     push();
+    //loop to draw pattern repeatedly
     for(var x = 105; x < width - 100; x += 200)
         for(var y = 205; y < height - 100; y += 200)
     {
         //console.log(i, x, y);
-        drawPattern(x, y, rot);
-        
-       
+        drawPattern(x, y, rot); 
     } 
-    pop();
-    
-
-   
-    
+    pop(); 
 }
 
 
-
+//function that draws my polygons and flower
 function drawPattern(startX, startY, rot) {
     push();
-    //    noStroke();
-
+    
     strokeWeight(3);
     stroke('black');
 
-    
+    //displace object by x and y, it's cumulative which is why I am using push() and pop() 
     translate(startX, startY);
     
     
     push();
-    if(rot) {
-        rotate(frameCount / -100.0);}
+        if(rot) {
+            rotate(frameCount / -100.0);
+                }
+    
         fill('yellow');
-
         polygon(0, 0, 100, 7);
     pop();
     
     push();
-    fill('orange');
-    if(rot) {
-    rotate((frameCount / -(100.0)));}
-    polygon(0, 0, 80, 7);
+        fill('orange');
+        if(rot) {
+            rotate((frameCount / -(100.0)));
+                }
+        polygon(0, 0, 80, 7);
     
     pop();
     
     push();
-    fill('yellow');
-    if(rot) {
-    rotate(frameCount / 50.0);}
-    var radius = 20 + startX/100.+ startY/80.;
-    polygon(0, 0, 50, 5);
+        fill('yellow');
+        if(rot) {
+            rotate(frameCount / 30.0);
+                }
+//        var radius = 20 + startX/100.+ startY/80.;
+        polygon(0, 0, 50, 5);
     
     
 
@@ -136,26 +141,42 @@ function polygon(x, y, radius, npoints) {
   endShape(CLOSE);  
 }
 
+
+
 function mousePressed() {
-    //check this is working
-//    console.log("mouse Pressed");
+    //check this function is working
+    console.log("mouse Pressed");
     
-    //loop through the bubbles and see if mouse inside
+    //check if mouse is inside 'play button', set rotation state to 1
     var  d; 
        d = dist(mouseX, mouseY, cirlceX, circleY);
         
         if(d < circleSize/2) {
             console.log("circle is clicked");
-            rot = 1;
-             
             
-             
+            if(buttonState === 1){
+                rot = 1;
+                buttonState=0;
+                buttonState2 = 1;
+            }
+            
+            else if (buttonState2 === 1){
+                rot = 0;
+                buttonState=1;
+                buttonState2 = 0;
+            }
+            
     }
+    
 }
+
+
 
 function mouseClicked() {
     console.log("mouse is clicked");
+    
     drawPattern(x, y, rot);
+    
 }
 
 
