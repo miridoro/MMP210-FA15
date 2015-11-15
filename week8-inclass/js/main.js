e90c808f5fe125759b95539942e8d5b3b451fd26
Miriam <miriam.wodrich@gmail.com>
2015-11-15 13:52:19 -0500

    adding final comments to midterm

--- week8-inclass/js/main.js
+++ week8-inclass/js/main.js
@@ -4,55 +4,59 @@ var circleX, circleY;
 var x, y;
 //variable that sets state of rotation, 0 stands for 'off' and 1 stands for 'on'
 var rot = 0;
-var buttonState = 1;
-var buttonState2 = 0;
+//setting the state of my 'play button' and 'pause button'
+var playButton = 1;
+var pauseButton = 0;
+var displaceBy = 70;
 
 
 
 function setup() {
     //create the Canvas
-    
-    createCanvas(windowWidth, windowHeight);
-    
-}
+    createCanvas(windowWidth, windowHeight); 
+}//end setup ()
 
 
 //function to draw the 'Play' button
-function drawButton(buttonState) {
-    cirlceX = 120 + 70;
+function drawPlayButton() {
+    cirlceX = 120 + displaceBy;
     circleY = 50;
     circleSize = 80;
     noStroke();
-    ellipse(120 + 70, 50, circleSize, circleSize);
+    ellipse(120 + displaceBy, 50, circleSize, circleSize);
     fill('black');
-    triangle(100 + 70, 75, 100 + 70, 25, 150 + 70, 50);
+    triangle(100 + displaceBy, 75, 100 + displaceBy, 25, 150 + displaceBy, 50);
     textSize(23);
     fill('black');
     text("Press Play", 15, 55);
-}
+}//end drawButton()
+
+
+
 
 //function to draw the 'Pause' button
-function drawButton2(buttonState2) {
+function drawPauseButton() {
     noStroke();
-    ellipse(120 + 70, 50, 80, 80);
+    ellipse(120 + displaceBy, 50, circleSize, circleSize);
     fill('black');
     rect(175, 25, 11, 50);
     rect(195, 25, 11, 50);
     textSize(23);
     text("Press Pause", 15, 55);
-}
+}//end drawButton2()
+
 
 //function that calls and runs drawButton() and drawPattern() functions 
 function draw() {
     background('orange');
     fill('white');
     
-    if(buttonState){
-    drawButton(buttonState);}
-//    drawButton2();
-
-    if(buttonState2){
-        drawButton2(buttonState2);
+    if(playButton){
+    drawPlayButton();
+    }
+    
+    if(pauseButton){
+        drawPauseButton();
     }
     
 
@@ -70,7 +74,7 @@ function draw() {
         drawPattern(x, y, rot); 
     } 
     pop(); 
-}
+}//end draw()
 
 
 //function that draws my polygons and flower
@@ -107,16 +111,11 @@ function drawPattern(startX, startY, rot) {
         if(rot) {
             rotate(frameCount / 30.0);
                 }
-//        var radius = 20 + startX/100.+ startY/80.;
         polygon(0, 0, 50, 5);
     
     
-
-    //A design for a simple flower
-//    translate(110, 110);
-    
    
-//    translate(startX, startY);
+//  my flower
     fill('white');
     for (var i = 0; i < 10; i++) {
         
@@ -125,7 +124,8 @@ function drawPattern(startX, startY, rot) {
     }
     pop();
      pop();
-}
+    
+}//end drawPattern()
 
 
 
@@ -137,9 +137,9 @@ function polygon(x, y, radius, npoints) {
     var sx = x + cos(a) * radius;
     var sy = y + sin(a) * radius;
     vertex(sx, sy);
-  }
+  }//end for loop
   endShape(CLOSE);  
-}
+}//end polygon()
 
 
 
@@ -154,29 +154,33 @@ function mousePressed() {
         if(d < circleSize/2) {
             console.log("circle is clicked");
             
-            if(buttonState === 1){
+            //if play button is clicked, start rotating shapes
+            if(playButton === 1){
                 rot = 1;
-                buttonState=0;
-                buttonState2 = 1;
-            }
+                playButton=0;
+                //pause button becomes visible when shapes rotate
+                pauseButton = 1;
+            }//end if loop
             
-            else if (buttonState2 === 1){
+            //if pause button is clicked, stop rotating the shapes
+            else if (pauseButton === 1){
                 rot = 0;
-                buttonState=1;
-                buttonState2 = 0;
-            }
+                //play button becomes visible when shapes do not rotate
+                playButton=1;
+                pauseButton = 0;
+            }//end else if loop
             
-    }
+    }//end of main if loop
     
-}
+}//end of mousePressed()
 
 
-
-function mouseClicked() {
-    console.log("mouse is clicked");
-    
-    drawPattern(x, y, rot);
-    
-}
+// just for testing purposes:
+//function mouseClicked() {
+//    console.log("mouse is clicked");
+//    
+//    drawPattern(x, y, rot);
+//    
+//}
 
 
