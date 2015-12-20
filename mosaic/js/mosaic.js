e3eb665090042789f14a22e8d5d6919ac7573427
Miriam <miriam.wodrich@gmail.com>
2015-12-20 12:09:44 -0500

    first final draft

--- mosaic/js/mosaic.js
+++ mosaic/js/mosaic.js
@@ -1,49 +1,163 @@
+var albumID;
+var song;
+var audio;
+var playing;
 
+
+// when the enter key is pressed, updateArtist function is called
 
 document.getElementById('myText').onkeypress = function (e) {
     if (!e) e = window.event;
     var keyCode = e.keyCode || e.which;
     //ASCII code for enter key
     if (keyCode == '13') {
-        // Enter pressed
-        updateArtist(document.getElementById("myText").value);
-    }
-}
+        //        updateArtist(document.getElementById("myText").value);
+        updateArtist($('#myText').val());
+    } //end if
+    
+} //end function
 
 
+
+
+// function accesses apple's iTunes API
+
+function updateArtist(artistname) {
+    var url = "https://itunes.apple.com/search?entity=album&term=" + artistname + "&callback=?";
+    //    console.log(url);
+
+    //updateData is a callback function that gets passed to be called later
+    $.getJSON(url, updateData);
+    
+} // end udateArtist function
 
 
 
 function updateData(data) {
-    //prints to concole
-    //    console.log(data);
-    
-    
+
+    $('#albumAnchor').empty();
+    playing = false;
 
 
     //set first album title to first element of the JSON data
 
-    for (var i = 0; i < Math.min(5, data.resultCount); i++) {
-        setTitle(i, data.results[i].collectionName);
-        setImage(i, data.results[i].artworkUrl100);
+    for (var i = 0; i < Math.min(data.resultCount); i++) {
+        createDiv(i);
+        
+        //        setTitle(i, data.results[i].collectionName);
 
+        setImage(i, data.results[i].artworkUrl100.replace("100x100", "250x250"));
+
+        var img = $('#div' + i + ' img')
+        img.data('albumId', data.results[i].collectionId);
+
+
+        img.click(coverClick.bind(img));
+
+        //console.log($('#div' + i + ' img').data('albumId'));
+
+    } //end for loop
+    
+} // end udateData function
+
+
+
+function updateSongID(array) {
+
+    for (var i = 0; i < array.length; i++) {
+
+        urlSong = ("https://itunes.apple.com/lookup?id=" + array[i] + "&entity=song" + "&callback=?");
+
+        var cover_id = "#div" + i + " .image"
+
+        $.getJSON(urlSong, function (data) {
+            var url = data.results[1].previewUrl;
+            console.log(cover_id + ": " + url);
+        });
+
+    }  // end for loop
+
+} // end updateSongID
+
+
+
+function updateSongData(data) {
+    setSong(data.results[1].previewUrl);
+    console.log(song);
+}
+
+
+//when its already true, just need to pause
+
+
+
+function coverClick() {
+    // window.open(url);
+    //console.log(this);
+    console.log(this.data("albumId"));
+
+
+
+
+    if (!playing) {
+
+        var this_id = this.data("albumId");
+        var urlSong = ("https://itunes.apple.com/lookup?id=" + this_id + "&entity=song" + "&callback=?");
+
+        $.getJSON(urlSong, function (data) {
+
+            var url = data.results[1].previewUrl;
+            audio = new Audio(url);
+            audio.play();
+            playing = true;
+
+        });
+    } else {
+        audio.pause();
+        playing = false;
     }
 
+
 }
 
 
-function updateArtist(artistname) {
-    var url = "https://itunes.apple.com/search?entity=album&term=" + artistname + "&callback=?";
 
-    //callback, a function you pass to get called later (ie button)
+function coverClick2() {
+    // window.open(url);
+    //console.log(this);
+    console.log(this.data("albumId"));
 
+    var this_id = this.data("albumId");
+
+    urlSong = ("https://itunes.apple.com/lookup?id=" + this_id + "&entity=song" + "&callback=?");
+
+    $.getJSON(urlSong, function (data) {
+        var url = data.results[1].previewUrl;
+        //        console.log(url);
+        audio = new Audio(url);
+
+
+        if (!playing) {
+            audio.play();
+            playing = true;
+        } else {
+            audio.pause();
+            playing = false;
+        }
+
+    });
+
+}
+
+
+// this function appends divs to the albumAnchor main div, which can be found in index.html
+
+function createDiv(n) {
+  
+    $('#albumAnchor').append('<div id="div' + n + '"' + ' class="album"><h2 class="title"></h2><img class="image" src="placeholder.jpg" alt="placeholder" width="200px"></div>');
     
-    $.getJSON(url, updateData);
 }
 
-
-
-//call function
 
 
 
@@ -52,21 +166,21 @@ function setValue(id, value) {                     
 }
 
 
-function setTitle(n, value) {
+function setAlbumID(n, value) {
+    $('#div' + n + ' img').data('albumId', value);
 
-    document.getElementById("title" + n).innerText = value;  
+}
+
+
+function setTitle(n, value) {
+    //    document.getElementById("title" + n).innerText = value;  
+    $('#div' + n + ' .title').text(value);
 }
 
 function setImage(n, value) {
-    document.getElementById("image" + n).src = value;
-    
+    //    document.getElementById("image" + n).src = value;
+    $('#div' + n + ' img').attr('src', value);
+
 }
 
-
-
-//function image(n, value) {
-//    var img = document.createElement("IMG");
-//    document.getElementById("imgageDiv" + n).appendChild(img);
-//    document.getElementById("image" + n).src = value;
-//}
 
--- mosaic/js/mosaic_bak.js
+++ mosaic/js/mosaic_bak.js
@@ -1,24 +1,3 @@
-//document.getElementById("myText").value = "Hello!";
-
-//$('body')
-//.append('Album Title')
-//.css('color', 'red').addClass('myClass');
-
-
-
-//var div = document.createElement('div');
-//div.innerHTML = "Album Title";
-//div.src = "../placeholder.jpg";
-//// set style
-//// better to use CSS though - just set class
-//div.setAttribute('class', 'myClass'); // and make sure myclass has some styles in css
-//document.body.appendChild(div);
-
-
-
-
-
-
 
 
 document.getElementById('myText').onkeypress = function (e) {
@@ -47,32 +26,7 @@ function updateData(data) {
     for (var i = 0; i < Math.min(5, data.resultCount); i++) {
         setTitle(i, data.results[i].collectionName);
         setImage(i, data.results[i].artworkUrl100);
-//        $( "#image1").append( "<div>Hello</div>" );
-        
-//        $("#foo").append(data.results[i].artworkUrl100);
-        
-//        var src = $('#image' + i).attr('src');
-//        var newDiv = document.createElement('div');
-////        newDiv.className='test';
-//$('#foo').append('<img src="' + src + '" />');
-           
-//        var newDiv = document.createElement('div');
-////        var title = document.getElementById("title" + i);
-//        var img = document.getElementById("image" + i);
-////        newDiv.appendChild(title);
-//        newDiv.appendChild(img);
-////        $( "#title" + i).append( setTitle(i, data.results[i].collectionName));
-//        document.body.appendChild(newDiv);
-       
-        
-//        setImage(i, $("#foo").append(data.results[i].artworkUrl100));
-        
-//        image(i, data.results[i].artworkUrl100);
-        
-//        var newelement = $('<div class="new">');
-//        $('#mydiv').append(newelement);
-//        //alt
-//        $('<div class="new">').appendTo("#mydiv");
+
     }
 
 }
@@ -100,37 +54,19 @@ function setValue(id, value) {                     
 
 function setTitle(n, value) {
 
-//    document.getElementById("title" + n).innerText = value;  
-    $('#title' + n).text(value);
-    console.log($('#title' + n).text());
+    document.getElementById("title" + n).innerText = value;  
 }
 
 function setImage(n, value) {
-//    document.getElementById("image" + n).src = value;
-    $('#image' + n).attr('src', value)
-//    console.log($('#image' + n).attr('src'));
+    document.getElementById("image" + n).src = value;
     
 }
 
 
 
-function image(n, value) {
-    var img = document.createElement("IMG");
-    document.getElementById("imgageDiv" + n).appendChild(img);
-    document.getElementById("image" + n).src = value;
-}
-
-
-
-
-
-//$("#foo").append("<div><img src=../placeholder.jpg></div>");
-
-
-//var cars = data.split(',');
-//for (var c in cars) {
-//    var newElement = document.createElement('div');
-//    newElement.id = cars[c]; newElement.className = "car";
-//    newElement.innerHTML = cars[c];
-//    document.body.appendChild(newElement);
+//function image(n, value) {
+//    var img = document.createElement("IMG");
+//    document.getElementById("imgageDiv" + n).appendChild(img);
+//    document.getElementById("image" + n).src = value;
 //}
+
